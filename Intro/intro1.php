77ee65cf40eb0307208dbf03a66c9c133adb81b7
david lash <dlash02@gmail.com>
2020-01-07 14:56:38 -0600

    THis is day 1 changed again

--- Intro/intro1.php
+++ Intro/intro1.php
@@ -1,10 +1,17 @@
 <html>
 <head>
    <title> This is a title </title>
+    <style>
+       #mySpan {
+           color: red;
+           font-size: 36px;
+       }
+    </style>
 </head>
 <body>
 <h2> Hello World!!!!</h2>
-The following is an ordered list
+<span id="mySpan"> The following is an ordered list </span>
+<span style="color:blue"> The following is an ordered list </span>
 <img src="nasa.jpg" alt="nasa image" height="36px" width="36px"/>
 <a href="http://aurora.edu"> The School </a>
 <ul>
@@ -13,5 +20,11 @@ The following is an ordered list
     <li> Hot dogs</li>
     <li> Pizza</li>
 </ul>
+<table border="1">
+   <tr> <th> Team </th> <th> Record</th> <th> Division </th></tr>
+    <tr> <td> Saints </td> <td> No More</td> <td> Soutd </td></tr>
+    <tr> <td> Bears </td> <td> No More</td> <td> Nortd </td></tr>
+    <tr> <td> Ravens </td> <td> 15 - 1 </td> <td> East </td></tr>
+</table>
 </body>
 </html>
