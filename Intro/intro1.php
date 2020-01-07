3dd57057ab6c26bd308131afbf70a47e766ecb80
david lash <dlash02@gmail.com>
2020-01-07 14:34:27 -0600

    This is day 1

---
+++ Intro/intro1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+   <title> This is a title </title>
+</head>
+<body>
+<h2> Hello World</h2>
+The following is an ordered list
+<img src="nasa.jpg" alt="nasa image" height="36px" width="36px"/>
+<a href="http://aurora.edu"> The School </a>
+<ul>
+    <li> Apples
+        <img src="http://45.55.136.114/~dlash/bach.jpg" alt="Bach" height="36px" width="36px"/> </li>
+    <li> Hot dogs</li>
+    <li> Pizza</li>
+</ul>
+</body>
+</html>
